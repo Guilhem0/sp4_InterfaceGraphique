8fcb1283c461e3d67a5511256155db8192ccda0e
Guilhem de Cambiaire <guilhem.decambiaire@epfedu.fr>
2022-12-06 11:43:30 +0100

    belle avancée dans les vidéos de l'interface, probleme au niveau l'implatation de notre code

--- src/super_puissance4/CelluleGraphique.java
+++ src/super_puissance4/CelluleGraphique.java
@@ -3,6 +3,9 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package super_puissance4;
+
+import java.awt.Graphics;
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 
 /**
@@ -10,6 +13,38 @@ import javax.swing.JButton;
  * @author guilh
  */
 public class CelluleGraphique extends JButton {
+
     CelluledeGrille celluleAssocier;
-    
+    ImageIcon img_vide = new javax.swing.ImageIcon(getClass().getResource("/Images/celluleVide.png"));
+    ImageIcon img_desint = new javax.swing.ImageIcon(getClass().getResource("/Images/desintegrateur.png"));
+    ImageIcon img_jetonJaune = new javax.swing.ImageIcon(getClass().getResource("/Images/jetonJaune.png"));
+    ImageIcon img_jetonRouge = new javax.swing.ImageIcon(getClass().getResource("/Images/jetonRouge.png"));
+    ImageIcon img_trouNoir = new javax.swing.ImageIcon(getClass().getResource("/Images/trouNoir.png"));
+
+    public CelluleGraphique(CelluledeGrille uneCellule) {
+        celluleAssocier = uneCellule;
+    }
+
+    @Override
+    public void paintComponent(Graphics G) {
+        super.paintComponent(G);
+        if (celluleAssocier.presenceTrouNoir()==true) {
+            setIcon(img_trouNoir);
+        } else if (celluleAssocier.presenceDesintegrateur()==true) {
+            setIcon(img_desint);
+        } else {
+            String couleur_jeton = celluleAssocier.lireCouleurDuJeton();
+            switch (couleur_jeton) {
+                case "vide":
+                    setIcon(img_vide);
+                    break;
+                case "rouge":
+                    setIcon(img_jetonRouge);
+                    break;
+                case "jaune":
+                    setIcon(img_jetonJaune);
+                    break;
+            }
+        }
+    }
 }
